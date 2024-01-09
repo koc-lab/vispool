419d046c9833028e254875cddef9a06a322eb472
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-01-09 03:09:02 +0300

    fix: update the main driver to fit the new version
    Update the main driver to fit the new `glue` submodule.

--- vispool/__main__.py
+++ vispool/__main__.py
@@ -1,48 +1,40 @@
 import os
 
 import lightning as L
+from datasets.utils.logging import disable_progress_bar
 
-from vispool.glue_datamodule import GLUEDataModule
-from vispool.glue_transformer import GLUETransformer
+from vispool.glue.datamodule import GLUEDataModule
+from vispool.glue.transformer import GLUETransformer
 
+disable_progress_bar()
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-# MODEL_NAME = "bert-base-uncased"
-MODEL_NAME = "distilbert-base-uncased"
-# MODEL_NAME = "albert-base-v2"
-
-TASK_NAME = "cola"
-# TASK_NAME = "mrpc"
-
-MAX_EPOCHS = 2
-SEED = 42
-
 L.seed_everything(42)
 
+# MODEL_CHECKPOINT = "distilbert-base-uncased"
+MODEL_CHECKPOINT = "bert-base-uncased"
+TASK_NAME = "mrpc"
+# TASK_NAME = "cola"
+
 dm = GLUEDataModule(
-    model_name_or_path=MODEL_NAME,
+    MODEL_CHECKPOINT,
     task_name=TASK_NAME,
+    batch_size=32,
+    num_workers=8,
 )
-dm.setup("fit")
 
 model = GLUETransformer(
-    model_name_or_path=MODEL_NAME,
-    num_labels=dm.num_labels,
-    eval_splits=dm.eval_splits,
-    task_name=dm.task_name,
-    train_batch_size=64,
+    MODEL_CHECKPOINT,
+    task_name=TASK_NAME,
+    batch_size=32,
 )
 
 trainer = L.Trainer(
-    max_epochs=MAX_EPOCHS,
     accelerator="auto",
-    devices=1,
-    logger=False,
+    devices="auto",
+    max_epochs=1,
+    deterministic=True,
 )
+
 trainer.fit(model, datamodule=dm)
-
-print("Validation with validation dataloader...")
-trainer.validate(model, dataloaders=dm.val_dataloader())
-
-print("Validation with datamodule...")
 trainer.validate(model, datamodule=dm)
+trainer.test(model, datamodule=dm)
