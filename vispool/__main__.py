8e4fa0da21f0c134ab0512a622f86b5620fcb9bc
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-01-10 22:36:02 +0300

    feat: traverse all glue tasks for base classifier
    Update main module to run base sequence classifier on all
    GLUE tasks.

--- vispool/__main__.py
+++ vispool/__main__.py
@@ -5,41 +5,38 @@ from dotenv import load_dotenv
 from pytorch_lightning.loggers import WandbLogger
 from transformers import logging as transformers_logging
 
-from vispool import WANDB_LOG_DIR
+from vispool import GLUE_TASKS, WANDB_LOG_DIR
 from vispool.glue.datamodule import GLUEDataModule
 from vispool.glue.transformer import GLUETransformer
 
 load_dotenv()
 transformers_logging.set_verbosity_error()
 disable_progress_bar()
-L.seed_everything(42)
 
 MODEL_CHECKPOINT = "distilbert-base-uncased"
-# MODEL_CHECKPOINT = "bert-base-uncased"
-TASK_NAME = "mrpc"
-# TASK_NAME = "cola"
+for task_name in GLUE_TASKS:
+    L.seed_everything(42)
+    dm = GLUEDataModule(
+        MODEL_CHECKPOINT,
+        task_name=task_name,
+        batch_size=32,
+        num_workers=8,
+    )
 
-dm = GLUEDataModule(
-    MODEL_CHECKPOINT,
-    task_name=TASK_NAME,
-    batch_size=32,
-    num_workers=8,
-)
+    model = GLUETransformer(
+        MODEL_CHECKPOINT,
+        task_name=task_name,
+    )
 
-model = GLUETransformer(
-    MODEL_CHECKPOINT,
-    task_name=TASK_NAME,
-)
+    logger = WandbLogger(project="vispool", save_dir=WANDB_LOG_DIR)
+    trainer = L.Trainer(
+        accelerator="auto",
+        devices="auto",
+        max_epochs=2,
+        logger=logger,  # type: ignore
+        deterministic=True,
+    )
 
-logger = WandbLogger(project="vispool", save_dir=WANDB_LOG_DIR)
-trainer = L.Trainer(
-    accelerator="auto",
-    devices="auto",
-    max_epochs=3,
-    logger=logger,  # type: ignore
-    deterministic=True,
-)
-
-trainer.fit(model, datamodule=dm)
-trainer.validate(model, datamodule=dm)
-wandb.finish()
+    trainer.fit(model, datamodule=dm)
+    trainer.validate(model, datamodule=dm)
+    wandb.finish()
