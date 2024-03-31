dff5251a5df21146a4265c01bc1fe062c11a1a20
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-03-31 16:41:18 +0300

    feat: add visualization supported version
    Extend VVGTransformer to support visualization of the model.

--- vispool/model/model.py
+++ vispool/model/model.py
@@ -128,3 +128,55 @@ class VVGTransformer(L.LightningModule):
             ],
         )
         return optimizer
+
+
+class VisualVVGTransformer(VVGTransformer):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.graphs: list[torch.Tensor] = []
+
+    def forward(self, **inputs: dict) -> tuple[Any, torch.Tensor]:
+        token_embs = self.encoder(**inputs)[0]
+        vvgs = get_vvgs(
+            token_embs,
+            vvg_type=self.vvg_type,
+            timeline=self.timeline,
+            weight_method=self.weight_method,
+            penetrable_limit=self.penetrable_limit,
+            directed=self.directed,
+            degree_normalize=self.degree_normalize,
+        )
+        logits = self.gcn(vvgs, token_embs)
+        return logits, vvgs
+
+    def training_step(self, batch: Mapping, batch_idx: int) -> dict:
+        input_dict = {"input_ids": batch["input_ids"], "attention_mask": batch["attention_mask"]}
+        logits, vvgs = self(**input_dict)
+        if batch_idx == 0:
+            self.graphs.append(vvgs)
+        if self.num_labels == 1:
+            logits = logits.squeeze()
+        loss = self.loss_fn(logits, batch["labels"])  # type: ignore
+        self.log("train/loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        return {"loss": loss}
+
+    def validation_step(self, batch: dict, batch_idx: int) -> None:
+        if self.trainer.global_step == 0 and self.define_metric is not None and isinstance(self.logger, WandbLogger):
+            self.logger.experiment.define_metric(self.define_metric, summary="max")
+
+        input_dict = {"input_ids": batch["input_ids"], "attention_mask": batch["attention_mask"]}
+        logits, _ = self(**input_dict)
+        labels = batch["labels"]
+        if self.num_labels == 1:
+            logits = logits.squeeze()
+            preds = logits
+        else:
+            preds = torch.argmax(logits, dim=-1)
+        loss = self.loss_fn(logits, labels)  # type: ignore
+
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        metric_dict = self.metric.compute(predictions=preds, references=labels)
+        if metric_dict is not None:
+            log_dict = {"loss": loss} if metric_dict is None else {"loss": loss, **metric_dict}
+            log_dict = {f"val/{k}": v for k, v in metric_dict.items()}
+            self.log_dict(log_dict, prog_bar=True)
